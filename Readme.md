3942e1d1d619c7ca417c06db49e3470d603aecbf
edcarter <edcarter@ualberta.ca>
2015-09-24 14:48:48 -0600

    Readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Elias Carter, 2015
+ProcNanny Process manager
---
+++ procnanny.c
@@ -0,0 +1,19 @@
+int main(){
+
+	//Kill Other ProcNanny Processes
+
+	//Read In Config File
+
+	//Set Up Log File
+
+	//Check What Processes out of config are actually running
+
+	//Fork Child Processes for each monitor
+
+	//Wait for mesages from child processes
+
+	//Clean up Children
+
+	//Log metadata and exit
+
+}
